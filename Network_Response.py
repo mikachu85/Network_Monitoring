acd53a578f4ed2497ec926812f8a58fba40b05fd
Damien Jeremiah <michelld@hotmail.com>
2016-11-29 12:41:54 +0100

    Hvis ikke det er kontakt til en maskin opprettes logg fil. Dette inkluderer informasjon om bygg, romnummer og rad.

--- Network_Response.py
+++ Network_Response.py
@@ -17,30 +17,25 @@ def data_loader(filename):
                     "datamaskinid": row[0].strip(), # Computer ID
                     "bygg": (row[1].strip()), # Building
                     "rom": (row[2].strip()), # Room number
-                    "rad": float(row[3].strip()) # Row
+                    "rad": (row[3].strip()) # Row
                 })
     return records
 
-# csv - data
 test_data = data_loader("Data/test.data")
-
 datamaskinid = [x['datamaskinid'] for x in test_data]
 bygg = [x['bygg'] for x in test_data]
 rom = [x['rom'] for x in test_data]
 rad = [x['rad'] for x in test_data]
 
-# for i in range(0, 5, 1):
-#     print '%18s, %5s, %5s, %1s' % (datamaskinid[i], bygg[i], rom[i], int(rad[i]))
 
 f = open('Log\NORESPONSE', 'w')
 
 # Response from computer lab rooms - iterate through test DATA
-for i in range(0,19, 1):
+for i in range(0,len(rad), 1):
     response = os.system("ping " + datamaskinid[i] + ' -n ' + '1')
     if response == 0:
         time.sleep(0)
     else:
-        f.write(datamaskinid[i] + ' NO CONNECTION TO ' + bygg[i] + ' ' + rom[i] + '\n')
-
+        f.write(datamaskinid[i] + ' Ingen tilkobling til ' + bygg[i] + ' ' + rom[i] + ', ' + rad[i] + '\n')
 
 
